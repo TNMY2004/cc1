3137a9b39296aa4002b8d1a0590d5e1a296fa98c
TNMY <95021453+TNMY2004@users.noreply.github.com>
2024-10-07 10:01:35 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main {
-printf("/nThis is a program from my github account & executing it google cloud console/n");
+printf("\nThis is a program from my github account & executing it google cloud console\n");
 return 0;
 }
